8217945cbb16b39c78aad60b6705c5e6c8b3e44e
chefcampbell89 <bacampbell89@gmail.com>
2026-03-14 18:47:25 -0400

    Update supabase.js
    trying to triage login problems

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_VFFaisB6eX-8r2l5o9MQ4A_KbmjqCuz'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
