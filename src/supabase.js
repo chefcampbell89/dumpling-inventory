6cf1251834869fb202e59985e0fa59b361e0f5e4
chefcampbell89 <bacampbell89@gmail.com>
2026-03-15 08:09:37 -0400

    Update supabase.js
    forever and ever

--- src/supabase.js
+++ src/supabase.js
@@ -3,15 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    persistSession: true,
-    storageKey: 'dumpling-auth',
-    storage: window.localStorage,
-    autoRefreshToken: true,
-    detectSessionInUrl: false,
-  }
-})
+export const supabase = createClient(supabaseUrl, supabaseKey)
 
 // ---- ITEMS ----
 
